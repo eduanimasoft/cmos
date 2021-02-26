3896daece667735bf6713c24d789105e6644e88f
Patrik Vavercak <patrik.vavercak@innovatrics.com>
2021-02-27 00:41:03 +0100

    Fix animations after QA

--- animations/cmos_intro/cmos_intro.js
+++ animations/cmos_intro/cmos_intro.js
@@ -9,30 +9,36 @@ function animate() {
     SVG.find('.complementary-nmos')
         .animate(animationDuration).fill(stressColor)
         .animate(animationDuration).fill('#0029FF');
-    SVG.find('.complementary-title')
-        .animate(animationDuration).fill(stressColor)
-        .animate(animationDuration).fill('#111010');
+    SVG.find('.comp-text-anim')
+        .opacity(1).animate(animationDuration).fill(stressColor)
+        .animate(animationDuration).fill('#111010').opacity(0);
+    SVG.find('.complementary-arrows')
+        .attr({'fill-opacity': 1}).animate(animationDuration).fill(stressColor)
+        .animate(animationDuration).fill('#111010').attr({ 'fill-opacity': 0 });
 
     SVG.find('.metal')
         .animate(animationDuration, 2 * animationDuration).fill(stressColor)
         .animate(animationDuration).fill('#111010');
-    SVG.find('.metal-title')
-        .animate(animationDuration, 2 * animationDuration).fill(stressColor)
-        .animate(animationDuration).fill('#111010');
+    SVG.find('.metal-text-anim')
+        .animate(1, 2 * animationDuration).opacity(1)
+        .animate(animationDuration).fill(stressColor)
+        .animate(animationDuration).fill('#111010').opacity(0);
 
     SVG.find('.oxid')
         .animate(animationDuration, 4 * animationDuration).fill(stressColor)
         .animate(animationDuration).fill('#C4C4C4');
-    SVG.find('.oxid-title')
-        .animate(animationDuration, 4 * animationDuration).fill(stressColor)
-        .animate(animationDuration).fill('#111010');
+    SVG.find('.oxid-text-anim')
+        .animate(1, 4 * animationDuration).opacity(1)
+        .animate(animationDuration).fill(stressColor)
+        .animate(animationDuration).fill('#111010').opacity(0);
 
     SVG.find('.semiconductor')
         .animate(animationDuration, 6 * animationDuration).fill(stressColor)
         .animate(animationDuration).fill('#77CAFF');
-    SVG.find('.semiconductor-title')
-        .animate(animationDuration, 6 * animationDuration).fill(stressColor)
-        .animate(animationDuration).fill('#111010');
+    SVG.find('.semic-text-anim')
+        .animate(1, 6 * animationDuration).opacity(1)
+        .animate(animationDuration).fill(stressColor)
+        .animate(animationDuration).fill('#111010').opacity(0);
 }
 
 animate();
